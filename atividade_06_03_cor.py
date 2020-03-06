48bca4fc64aa8ca82ef1d9677a4c38f69c48f020
Logon Aluno <logonaluno@local.com>
2020-03-06 09:35:04 -0300

    AULAS

--- atividade_06_03_cor.py
+++ atividade_06_03_cor.py
@@ -1,15 +1,15 @@
 nome = input("Informe o nome do paciente \n")
 idade = int(input("Informee a idade \n"))
 
-gpr = input("O paciente eh de risco ? (S)im ou (N)ao")
+gpr = input("O paciente eh de risco ? (S)im ou (N)ao").upper()
 if gpr != "S" and gpr != "N":
     gpr = input("O paciente eh de risco ? (S)im ou (N)ao \n Use SOMENTE S ou N")
 
 #gpr = input("O paciente é ja teve ou tem alguma doença grave realiconada "
 #                +"ao sistema respiratorio? se sim, qual? (informe N para não)")
+#gpr = gpr.upper()
 
 nome = nome.title()
-gpr = gpr.upper()
 
 if idade <15:
     print("Pessoa prioritaria")
