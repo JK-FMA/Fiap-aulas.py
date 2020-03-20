93a792e579668c51d8f66556747ce2b79016f37a
JK-FMA <61872502+JK-FMA@users.noreply.github.com>
2020-03-20 02:49:46 +0000

    Add files via upload

---
+++ home_06_03_ADM_v2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+user = input("Informe o seu nivel de usuario. (ADM, USR, GUEST) \n").upper()
+sex = input("Informe o seu sexo \n (H)omem	(M)ulher \n").upper()
+i = int(1)
+
+while sex != "H" and sex != "M" :
+	
+	sex = input("Informe somente H para homem ou M para mulher \n").upper()
+
+if sex == "H":
+	if user == "ADM":
+		print ("Ola administrador")
+	elif user == "USR" :
+		print ("Ola usuario")
+	elif user == "GUEST":
+		print ("Ola convidado")
+	else:
+		print ("Ola desconhecido")
+else:
+	if user == "ADM":
+                print ("Ola administradora")
+	elif user == "USR" :
+                print ("Ola usuaria")
+	elif user == "GUEST":
+                print ("Ola convidada")
+	else:
+                print ("Ola desconhecida")
